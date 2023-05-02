29cb2e9ea928369df8e64cbb30761f9247c1e8a7
tgartland89 <thomas.gartland@gmail.com>
2023-05-02 16:36:42 -0600

    customer scopes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(newFavorite){
+    bestCustomer = newFavorite
+}
+
+const leastFavoriteCustomer = "bob"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not bob'
+}
